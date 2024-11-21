2d127a15a2676633422ba8fa1d22834c5c660434
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-11-21 21:48:25 +0530

    cors changes

--- socket/index.js
+++ socket/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   },
 });
