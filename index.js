92163f0e9e5228d05fbf9738e81b01c40f4c5608
Urvish Prajapati <urvishmeghani111@gmail.com>
2024-11-23 13:54:46 +0530

    updated cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { app, server } = require("./socket/index");
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://chatify-by-urvish.vercel.app"],
     credentials: true,
   })
 );
--- socket/index.js
+++ socket/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://chatify-by-urvish.vercel.app"],
     credentials: true,
   },
 });
